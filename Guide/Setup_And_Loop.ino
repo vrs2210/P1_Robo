abab7c4e9c853563a18fbf844d265d9458c510c2
Roland <Sejenicklas2005@gmail.com>
2025-11-21 09:58:18 +0100

    Add setup and loop functions for Arduino sketch

---
+++ Guide/Setup_And_Loop.ino
@@ -0,0 +1,9 @@
+// Runs when the board is turned on.
+void setup() {
+  // Recommended to initialize variables and pins in here.
+}
+
+// Loops the code inside consecutively. 
+void loop() {
+  // Recommended to run the actual board instructions in here, like button presses or readings.
+}
